f3ca58de89707eb234fed3ab805e50a347283cf7
Alexandr N <naberezhnyy.an@gmail.com>
2023-03-03 10:16:50 +0100

    example with const

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,27 @@
 <script lang="ts">
-	import { onMount } from "svelte";
+	import { onMount } from 'svelte';
 
-	let a = 1 as number
 	onMount(() => {
 		// Type Assertation
 		// Syntax
-		let element = <HTMLDivElement>document.getElementById('qwe')
+		let element = <HTMLDivElement>document.getElementById('qwe');
 		// or
-		let dick = document.getElementById('qwe') as HTMLDivElement
+		let dick = document.getElementById('qwe') as HTMLDivElement;
+	});
 
-	})
+	interface Req {
+		url: string;
+		method: 'GET' | 'POST';
+	}
+
+
+	function handleRequest(url:string,method: 'GET'|'POST'){}
+
+	const req = { url: 'https://example.com', method: 'GET' } as const;
+
+	handleRequest(req.url, req.method);
 </script>
 
-<div id='qwe'></div>
+<div id="qwe" />
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
