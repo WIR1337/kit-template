95d74dc54bff91e876d8e252ac39b8449dc2d8b6
Alexandr N <naberezhnyy.an@gmail.com>
2023-03-03 08:52:24 +0100

    add type assertation syntax

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	let a = 1 as number
+	onMount(() => {
+		// Type Assertation
+		// Syntax
+		let element = <HTMLDivElement>document.getElementById('qwe')
+		// or
+		let dick = document.getElementById('qwe') as HTMLDivElement
+
+	})
+</script>
+
+<div id='qwe'></div>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
